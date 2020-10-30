f186480c653885dbf0594a0f2c29fd59591253f7
Alice Chang <sinsofangels@gmail.com>
2020-10-30 11:18:39 -0400

    Add fetch data and custom hook

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,11 @@
 // write your custom hook here to control your checkout form
+import React, {useState} from "react";
+const useForm = param => {
+  const [value, setValue] = useState(param);
+  const handleChanges = param => {
+    setValue(param);
+  } 
+  return [value, setValue, handleChanges];
+}
+
+export default useForm;
